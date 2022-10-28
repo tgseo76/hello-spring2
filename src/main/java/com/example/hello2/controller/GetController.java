34c58ead1edb1c23b3b2843c2df0fb7f391eb6e6
tjxor <seotaegeon@gmail.com>
2022-10-28 11:33:24 +0900

    GetController log.info 추가

--- src/main/java/com/example/hello2/controller/GetController.java
+++ src/main/java/com/example/hello2/controller/GetController.java
@@ -1,26 +1,33 @@
 package com.example.hello2.controller;
 
 import com.example.hello2.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
+
 public class GetController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String getHello(){
+    public String getHello()
+    {log.info("hello로 오청");
         return "Hello world";
     }
 
     @GetMapping(value = "/name")
-    public String getName(){
+    public String getName()
+    {
+        log.info("name로 오청");
         return "taegeon";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
+        log.info("val로 오청");
         return variable;
     }
 
